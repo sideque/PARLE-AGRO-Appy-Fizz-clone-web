849b95e71ab2b2cc7550cfbadadfaee79b8698d1
Sidhique <xidhique@gmail.com>
2024-12-19 16:55:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-let elem = document.getElementById('rotate');
-window.addEventListener('scroll',() => {
-    let scrollposition = window.scrollY;
-    let rotate = scrollposition *0.35;
-    elem.style.transform = `rotate(${rotate}deg)`;
-})
+
